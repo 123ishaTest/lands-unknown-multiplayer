058c81614324168ab41e90ed166d938ba449a0fc
Isha Dijcks <ishadijcks@gmail.com>
2022-09-19 01:23:03 +0200

    Fix frontend project structure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
+import {Player} from "common/Player";
 
 createApp(App).mount('#app')
+
+const player = new Player("asd", "asd");
+
+console.log(player);
