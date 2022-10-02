357e87ac25e6dc5cb887b71c7a48f07e821a11fd
Isha Dijcks <ishadijcks@gmail.com>
2022-10-03 01:49:46 +0200

    Allow Heroku to set the port

--- backend/src/GameServer.ts
+++ backend/src/GameServer.ts
@@ -43,7 +43,7 @@ export class GameServer {
         }));
         app.use(express.json())
 
-        const PORT = 3000
+        const PORT = process.env.PORT ?? 3000
         app.listen(PORT, () => {
             console.log(`Server listening at http://localhost:${PORT}`)
         })
