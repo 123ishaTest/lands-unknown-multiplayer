e9e7cef3776be63d37bc6b50251092d41ee5302a
Isha Dijcks <ishadijcks@gmail.com>
2022-10-06 23:05:17 +0200

    Improve bank interaction logic

--- frontend/src/components/features/bank/Bank.vue
+++ frontend/src/components/features/bank/Bank.vue
@@ -10,8 +10,6 @@ const props = defineProps<{
   bank: Bank
 }>()
 
-const selectedSlotIndex = ref(0);
-
 const selectedAmount = ref(1);
 const amountOptions = ref([
   {text: '1x', value: 1},
@@ -23,14 +21,6 @@ const amountOptions = ref([
 const slots = computed(() => {
   return props.bank.slots;
 });
-
-const selectedSlot = computed(() => {
-  return slots.value[selectedSlotIndex.value]
-})
-
-const selectItem = (index: number) => {
-  selectedSlotIndex.value = index;
-};
 
 const withdrawItems = (id: ItemId, amount: number) => {
   ApiClient.send(new WithdrawItemByIdRequest(), {
@@ -47,15 +37,13 @@ const withdrawItems = (id: ItemId, amount: number) => {
       <option v-for="option in amountOptions" :value="option.value">{{ option.text }}</option>
     </select>
 
-    <button @click="withdrawItems(selectedSlot.item.id, 1)" class="border-2 border-black p-2 m-2">Withdraw</button>
 
     <div class="flex flex-row">
       <div class="flex flex-row flex-wrap justify-center sm:justify-start">
         <div v-for="(slot, index) in slots" :key="index + '-' + slot.item.id">
           <BankSlot :slot="slot"
-                    :is-selected="index === selectedSlotIndex"
                     :index="index"
-                    @click.native="selectItem(index)"
+                    @click.native="withdrawItems(slot.item.id, selectedAmount)"
           ></BankSlot>
         </div>
       </div>
