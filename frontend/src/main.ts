1fe3de537e42aea26a4234d9567f357598bfb26c
Isha Dijcks <ishadijcks@gmail.com>
2022-10-14 15:14:54 +0200

    Login with empty token on frontend when in dev mode

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,8 @@ import App from './App.vue'
 
 import './assets/main.css'
 import {FirebaseHelper} from "@/model/FirebaseHelper";
+import {ApiClient} from "@/model/ApiClient";
 
 createApp(App).mount('#app')
 
-FirebaseHelper.init();
+process.env.NODE_ENV === "production" ? FirebaseHelper.init() : ApiClient.login("");
