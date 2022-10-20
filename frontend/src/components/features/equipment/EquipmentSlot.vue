67dbcbdd07676840bcccab9320f3eae34e3a828a
Isha Dijcks <ishadijcks@gmail.com>
2022-10-20 17:23:52 +0200

    Fix display of equipment slots

--- frontend/src/components/features/equipment/EquipmentSlot.vue
+++ frontend/src/components/features/equipment/EquipmentSlot.vue
@@ -3,18 +3,28 @@ import {EquipmentType} from "common/features/equipment/EquipmentType";
 import {Equipment} from "common/features/equipment/Equipment";
 import {ApiClient} from "@/model/ApiClient";
 import {UnEquipItemRequest} from "common/api/inventory/UnEquipItemRequest";
+import {computed} from "vue";
 
 const props = defineProps<{
   type: EquipmentType
-  item: Equipment
+  item?: Equipment
 }>()
 
 const backgroundImage = () => {
   return new URL(`/src/assets/generated/equipment/${props.type}.png`, import.meta.url).href;
 };
 
+const itemImage = () => {
+  return new URL(`/src/assets/generated/items/${props.item?.image}.png`, import.meta.url).href;
+};
+
+
+const hasItem = computed(() => {
+  return props.item != undefined;
+});
+
 function unEquip() {
-  if(!props.item) {
+  if (!props.item) {
     return;
   }
   ApiClient.send(new UnEquipItemRequest(), {
@@ -25,14 +35,20 @@ function unEquip() {
 </script>
 
 <template>
-  <div class="h-24 w-24 border-2 rounded bg-amber-900" :title="type">
+  <div class="h-24 w-24 border-2 rounded bg-gray-600" :title="type">
     <div class="flex flex-row items-center justify-center p-3">
-      <img style="width: 64px; height: 64px;"
+
+      <img v-if="hasItem" style="width: 64px; height: 64px;"
+           @click="unEquip()"
+           class="pixelated"
+           :src="itemImage()"
+           :alt="item.name"
+      >
+      <img v-else style="width: 64px; height: 64px;"
            class="pixelated opacity-30"
            :src="backgroundImage()"
            :alt="type"
       >
-      <p @click="unEquip()">{{item?.name}}</p>
     </div>
   </div>
 
